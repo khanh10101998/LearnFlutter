651d0197f6aeec39c220c20e83e12ff694537c00
Khanh Nguyen Hong <khanhnguyenhong@Khanhs-MacBook-Pro.local>
2020-04-08 16:57:22 +0700

    update statefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -35,11 +35,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
         /*3*/
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41')
+        FavoriteWidget(),
       ],
     ),
   );
@@ -111,6 +107,52 @@ class MyApp extends StatelessWidget {
             textSection],
         ),
       ),
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+
+
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -=1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount +=1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build (BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            onPressed: _toggleFavorite,
+            color: Colors.red,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
     );
   }
 }
