8ade0f49b8a8c5d7a2eeb84b2e013fba89992f2e
Khanh Nguyen Hong <khanhnguyenhong@Khanhs-MacBook-Pro.local>
2020-04-16 16:18:14 +0700

    Complete learn girdView

--- lib/main.dart
+++ lib/main.dart
@@ -3,156 +3,54 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: [
-        Expanded(
-          /*1*/
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              /*2*/
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Oeschinen Lake Campground',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Text('demo'),
-
-              Text(
-                'Kandersteg, Switzerland',
-                style: TextStyle(
-                  color: Colors.blue,
-                ),
-              ),
-            ],
-          ),
-        ),
-        /*3*/
-        FavoriteWidget(),
-      ],
-    ),
-  );
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget textSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Text(''
-        'Nguyen hong khanh de mo thoi ne haha'
-        'de mo dong thu hai ne'
-        'demo dong thu ba ne'
-        'demo có dấu nè',
-    softWrap: true,
-    ),
-  );
-
-
   @override
   Widget build(BuildContext context) {
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Container(
-      child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-          _buildButtonColumn(color, Icons.call, 'CALL'),
-          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-          _buildButtonColumn(color, Icons.share, 'SHARE'),
-      ],
-    ),
+    // TODO: implement build
+    return new MaterialApp(
+      title: 'title ne',
+      home: new MainPage(title: 'Demo title ne', body: 'demo body ne',),
     );
+  }
+}
 
-    return MaterialApp(
-      title: 'Demo title ne',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter layout demo'),
-        ),
-        body: ListView(
-          children: [
-            Image.asset(
-              'images/hot_girl.jpg',
-              width: 400,
-              height: 500,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection],
-        ),
+class MainPage extends StatefulWidget {
+  final String title;
+  final String body;
+  MainPage({this.title, this.body}) : super();
+
+  MainPageState createState() => MainPageState();
+}
+
+class MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new GridView.extent(
+          maxCrossAxisExtent: 200.0,
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 5,
+        padding: const EdgeInsets.all(5),
+        children: _buildGridTiles(29),
       ),
     );
   }
 }
 
-class FavoriteWidget extends StatefulWidget {
-  @override
-  _FavoriteWidgetState createState() => _FavoriteWidgetState();
-
-
-}
-
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = true;
-  int _favoriteCount = 41;
-
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorited) {
-        _favoriteCount -=1;
-        _isFavorited = false;
-      } else {
-        _favoriteCount +=1;
-        _isFavorited = true;
-      }
-    });
-  }
-
-  @override
-  Widget build (BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
-            onPressed: _toggleFavorite,
-            color: Colors.red,
-          ),
+List<Widget> _buildGridTiles(numOfTiles){
+    List<Container> containers  = new List<Container>.generate(numOfTiles, (int index) {
+      final image = 'images/hot_girl.jpg';
+      return new Container(
+        child: new Image.asset(
+            image,
+          fit: BoxFit.cover,
         ),
-        SizedBox(
-          width: 18,
-          child: Container(
-            child: Text('$_favoriteCount'),
-          ),
-        ),
-      ],
-    );
-  }
+      );
+    }
+  );
+
+    return containers;
 }
